f8527c33831edbc1be5db6d3a84dfb791b537438
tFury <thomas_haenig@gmx.de>
2018-12-24 00:59:51 +0100

    code cleanup

--- src/app/partials/loadBabylon/loadBabylon.component.ts
+++ src/app/partials/loadBabylon/loadBabylon.component.ts
@@ -10,27 +10,6 @@ import {
 } from "@angular/core";
 //#endregion
 
-//#region LOGGER
-import {
-    Logger,
-    ELoglevel,
-    ETransportType
-} from "letslog";
-
-const logger = new Logger({
-    baseComment: "loadBabylon.component.ts",
-    loglvl: ELoglevel.DEBUG,
-    transports: [
-        {
-            showBaseComment: true,
-            showDate: false,
-            showLoglevel: true,
-            type: ETransportType.console
-        }
-    ]
-});
-//#endregion
-
 @Component({
     selector: "main",
     templateUrl: "./loadBabylon.component.html",
@@ -47,16 +26,17 @@ export class LoadBabylonComponent implements OnInit {
     ngOnInit() {
 
         this._babylonModel = new BabylonModel(this.babylonElement);
-
         let loader = new babylon.AssetsManager(this._babylonModel.scene);
         let task = loader.addMeshTask("complexeCube", "", "assets/", "complexeCube.babylon");
         babylon.OBJFileLoader.OPTIMIZE_WITH_UV = true;
 
         task.onSuccess = (object) => {
 
-            for (const iterator of object.loadedMeshes) {
-                iterator.position = new BABYLON.Vector3(0, -9, 0);
-                iterator.checkCollisions = true;
+            for (const mesh of object.loadedMeshes) {
+                console.log("mesh", object);
+
+                mesh.position = new BABYLON.Vector3(0, -9, 0);
+                mesh.checkCollisions = true;
             }
             console.log("onSuccess", object);
         };
--- src/index.ts
+++ src/index.ts
@@ -2,11 +2,6 @@
 import "core-js/es7/reflect";
 import "zone.js/dist/zone";
 import "core-js/es6/reflect";
-
-// import "three/examples/js/loaders/OBJLoader";
-// import "three/examples/js/loaders/MTLLoader";
-// import "three/examples/js/loaders/GLTFLoader";
-// import "three/examples/js/loaders/ColladaLoader";
 import "babylonjs-loaders";
 
 import { platformBrowserDynamic } from "@angular/platform-browser-dynamic";
@@ -16,4 +11,3 @@ import { AppModule } from "./app/app.module";
 
 platformBrowserDynamic().bootstrapModule(AppModule)
     .catch(err => console.error(err));
-    
